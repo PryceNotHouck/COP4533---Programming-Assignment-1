4fdd9acbb84ab5faccb7c2eb943d40d698872440
Pryce Houck <phouck@ufl.edu>
2026-01-29 15:17:48 -0500

    started generating input samples

---
+++ Scalability.py
@@ -0,0 +1,20 @@
+# x-axis = n
+# y-axis = runtime
+
+from Matcher import matcher
+import random
+
+def make_input(n):
+    output = f"{n}\n"
+
+    proposals = []
+    recipients = []
+    for i in range(n):
+        row = [j for j in range(1, n + 1)]
+        random.shuffle(row)
+        print(row)
+
+    return output
+
+
+make_input(3)
