a335cbead521812c6bac55bec410260bcbbcd057
Pryce Houck <phouck@ufl.edu>
2026-01-29 17:52:04 -0500

    finished scalability

--- pa1/Scalability.py
+++ pa1/Scalability.py
@@ -1,34 +1,40 @@
-# x-axis = n
-# y-axis = runtime
-
-from Matcher import matcher
+import copy
+import Matcher
+from matplotlib import pyplot as plt
+import time
+import numpy as np
+import math
 import random
 
 def make_input(n):  # O(n!) (not part of matcher/verifier so that's fine)
-    output = f"{n}\n"
+    output = f"\n{n}\n"
 
     proposer_inputs = []
     recipient_inputs = []
     for i in range(n):
         row = [j for j in range(1, n + 1)]
         while row in proposer_inputs:
+            random.seed(time.time())
             random.shuffle(row)
-        proposer_inputs.append(row)
+        proposer_inputs.append(copy.deepcopy(row))
 
+        random.seed(time.time())
+        random.shuffle(row)
         while row in recipient_inputs:
+            random.seed(time.time())
             random.shuffle(row)
-        recipient_inputs.append(row)
+        recipient_inputs.append(copy.deepcopy(row))
 
     for row in proposer_inputs:
-        for index, value in enumerate(row):
-            output += f"{value}"
+        for index, value_p in enumerate(row):
+            output += f"{value_p}"
             if index != len(row) - 1:
                 output += " "
         output += "\n"
 
     for index_i, row in enumerate(recipient_inputs):
-        for index_j, value in enumerate(row):
-            output += f"{value}"
+        for index_j, value_r in enumerate(row):
+            output += f"{value_r}"
             if index_j != len(row) - 1:
                 output += " "
         if index_i != len(row) - 1:
@@ -38,6 +44,27 @@ def make_input(n):  # O(n!) (not part of matcher/verifier so that's fine)
 
 
 if __name__ == '__main__':
-    n = 5
-    test_input = make_input(n)
-    print(test_input)
+    sizes = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536]
+    times = []
+    for n in sizes:
+        input_text = make_input(n)
+        recipients, proposers = Matcher.format_preferences(input_text)
+        start = time.perf_counter()
+        temp = Matcher.matcher(recipients = recipients, proposers = proposers)
+        end = time.perf_counter()
+        times.append(end - start)
+
+    plt.scatter(sizes, times, color = 'red')
+
+    sizes = np.array(sizes)
+    times = np.array(times)
+    z = np.polyfit(x = sizes, y = times, deg = 1000)
+    p = np.poly1d(z)
+    plt.plot(sizes, p(sizes))
+
+    plt.title('Gale-Shapely Matcher Scalability')
+    plt.xlabel("Input Size (n)")
+    plt.ylabel("Runtime (s)")
+    plt.legend()
+
+    plt.show()
