1b22769fbdc9f3eddf2fbfc69bd6e8c849bbafdf
Pryce Houck <phouck@ufl.edu>
2026-01-29 15:44:05 -0500

    input generator

--- pa1/Scalability.py
+++ pa1/Scalability.py
@@ -4,7 +4,7 @@
 #from Matcher import matcher
 import random
 
-def make_input(n):
+def make_input(n):  # O(n!) (not part of matcher/verifier so that's fine)
     output = f"{n}\n"
 
     proposer_inputs = []
@@ -19,9 +19,25 @@ def make_input(n):
             random.shuffle(row)
         recipient_inputs.append(row)
 
+    for row in proposer_inputs:
+        for index, value in enumerate(row):
+            output += f"{value}"
+            if index != len(row) - 1:
+                output += " "
+        output += "\n"
+
+    for index_i, row in enumerate(recipient_inputs):
+        for index_j, value in enumerate(row):
+            output += f"{value}"
+            if index_j != len(row) - 1:
+                output += " "
+        if index_i != len(row) - 1:
+            output += "\n"
+
     return output
 
 
 if __name__ == '__main__':
-    n = 3
-    input = make_input(n)
+    n = 5
+    test_input = make_input(n)
+    print(test_input)
