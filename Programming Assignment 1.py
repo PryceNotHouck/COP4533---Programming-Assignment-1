d0f29988c8a42627b21b316a959463c35c9b07d4
Pryce Houck <phouck@ufl.edu>
2026-01-29 14:06:17 -0500

    matcher version 1

--- Programming Assignment 1.py
+++ Programming Assignment 1.py
@@ -20,7 +20,7 @@ for i in range(1, len(split) // 2, count):  # O(n)
     for j in range(0, len(row)):
         temp.append((int(row[j]), j + 1))
     hospitals.append(temp)
-print(hospitals)
+print("Hospitals:", hospitals)
 
 # applicant
 for i in range(len(split) // 2 + 1, len(split), count):  # O(n)
@@ -29,6 +29,7 @@ for i in range(len(split) // 2 + 1, len(split), count):  # O(n)
     for j in range(0, len(row)):
         temp.append((int(row[j]), j + 1))
     applicants.append(temp)
+print("Applicants:", applicants)
 
 # while (some hospital is free and hasn't been matched to every applicant) {
     # Choose such a hospital h
@@ -42,49 +43,62 @@ for i in range(len(split) // 2 + 1, len(split), count):  # O(n)
     #   a rejects h
 # }
 
-def matcher(applicants, hospitals):
+def matcher(recipients, proposers):
     pairs = []
-    unpaired_h = []
-    unpaired_a = []
-    for i in range(0, len(hospitals)):
-        unpaired_h.append(i + 1)
-        unpaired_a.append(i + 1)  # same length and indexes
-    print(unpaired_h)
+    unpaired_p = []
+    unpaired_r = []
+    for i in range(0, len(proposers)):
+        unpaired_p.append(i + 1)
+        unpaired_r.append(i + 1)  # same length and indexes
 
-    while (len(unpaired_h) > 0):
-        h = unpaired_h[0]
+    h_choice = 1
+    num_proposals = 0
+    while len(unpaired_p) > 0:
+        if h_choice > len(recipients):
+            raise IndexError("h has exhausted possible choices")
+
+        h = unpaired_p[0]
         a = -1
-        for pref in hospitals[h - 1]:  # O(1)
-            if pref[1] == 1:
+        for pref in proposers[h - 1]:  # O(1)
+            if pref[1] == h_choice:
                 a = pref[0]
+                break
         if a == -1:
-            raise ValueError("h has no first preference")
+            raise ValueError("h has no selected preference")
 
-        # if (a is unpaired)
-            # pairs.append([ h , a ])
-            # unpaired_h.pop(0)
-            # unpaired_a.remove(a)
-        # else:
-            # if (a prefers h):
-                # find pair for a in pairs
-                # set h` in pair as h
-                # unpaired_h.append(h)
-            # else:
-                # continue
-
-        if a in unpaired_a:
+        num_proposals += 1
+        if a in unpaired_r:
             pairs.append([h, a])
-            unpaired_h.pop(0)
-            unpaired_a.remove(a)
+            unpaired_p.pop(0)
+            unpaired_r.remove(a)
+            h_choice = 1
         else:
-            for pref in applicants[a - 1]:
-                if pref[0] == h:
-                    current = pref[1]
-            if (a == 1):
-                pass
-            else:
-                continue  # reject
+            current_p = -1
+            current_p_pref = -1
+            candidate_p_pref = -1
+            pair_i = -1
 
+            for i in range(len(pairs)):
+                if pairs[i][1] == a:
+                    current_p = pairs[i][0]
+                    pair_i = i
+                    break
+            for pref in recipients[a - 1]:
+                if pref[0] == h:
+                    candidate_p_pref = pref[1]
+                if pref[0] == current_p:
+                    current_p_pref = pref[1]
+                if candidate_p_pref != -1 and current_p_pref != -1:
+                    break
+
+            if candidate_p_pref < current_p_pref:
+                pairs[pair_i][0] = h
+                unpaired_p.append(current_p)
+                h_choice = 1
+            else:
+                h_choice += 1
+                continue  # reject
+    print("Number of Proposals:", num_proposals)
     return pairs # [h, a]
 
 def verifier(applicants, hospitals, output):
@@ -95,5 +109,12 @@ def verifier(applicants, hospitals, output):
     return checked
 
 if __name__ == '__main__':
-    output = matcher(applicants, hospitals)
-    verifier(applicants, hospitals, output)
+    matchings = matcher(recipients = applicants, proposers = hospitals)
+    for match in matchings:
+        print(match[0], match[1])
+    # 1 2
+    # 2 3
+    # 3 1
+    # [[1, 2], [2, 3], [3, 1]]
+
+    verifier(applicants, hospitals, matchings)
